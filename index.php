74cb564f82bb7ab753782ed6a4cf0657c89a4e04
duhodji <707162507+duhodji@users.noreply.github.com>
2020-12-07 22:55:14 -0500

    added css

--- index.php
+++ index.php
@@ -1,117 +1,39 @@
 <!DOCTYPE html>
-<html>
- <?php
-   $prevbg = $_GET['bg'];
-   $iter = $_GET['iter'];
-   if (empty($_GET['iter'])){
-     $iter = 0;
-   } else {
-     $iter = $_GET['iter'];
-   }
-   $opt = $_GET['opt'];
-   $options = array("sharpseekentry","sharpentry","sizentry",
-                    "numeraria","numerana","numeranto",
-                    "numeriana","numerary","measurary",
-                    "numerama","oblyq");
-   $backgrounds = array("#FFFFFF", "#D9D9D9", "#A6A6A6");
-   $background = rand(0,2);
-   while (($background == $prevbg)) {
-      $background = rand(0,2);
-   }
-   $mystyle = "background-color:" . $backgrounds[$background];
-   echo "<body style = " . $mystyle . ">";
- ?>
- <center>
+<html> 
+
+ <head>
   <?php
-   switch ($iter) {
-    case 0:
-     echo "You received this link because I want you to be part of a story. A story that needs a name. <br>";
-     echo "Picture in your mind an app that lets you measure what would otherwise be unmeasurable. <br>";
-     echo "An app that uses alternative methods to put a number in things like <br>";
-     echo "risk levels, future cashflows, or number of friends who will come to your party. <br>";
-     echo "Think of figure, values, quantities, sizes. Think of lateral thinking. <br>";
-     echo "You don't know exactly how it works, but can you picture it?  <br>";
-     echo "Now it is time to give it a name. <br>";
-     echo "<br>";
-     echo "Between the two names suggested below, which one conveys best the app you just imagined? <br>";
-     echo "(click on you preferred choice) <br>";
-     echo "<br>";
-     break;
-     case 1:
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo $options[$opt] . " was a good choice.<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "How about between these two? <br>";
-      echo "(Click on your preferred choice) <br>";
-      echo "<br>";
-      break;
-     case 2:
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "Thank you. This is really helpful. Please do it again. <br>";
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "Between the two names below, which one do you find best for a quantitative app? <br>";
-      echo "(Click on your preferred choice) <br>";
-      echo "<br>";
-     break;
-     case 3:
-      echo "<br>";
-      echo "<br>";
-      echo "You got this. Now keep doing it for as long as you are comfortable. <br>";
-      echo "I will combine your answers with the answers of other friends to find what name has the most appeal. <br> <br>";
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "Now keep doing this for as long as you are comfortable. <br>";
-      echo "(Click on your preferred choice) <br>";
-      break;
-     case 11:
-      echo "<br>";
-      echo "<br>";
-      echo "I think I have enough answers from you. Thank you. <br>";
-      echo "But feel free to continue if you want to.. <br>";
-      echo "When you are done, just close this browser window <br> <br>";
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "Which name works best for a quantitative app? <br>";
-      echo "(Click on your preferred choice) <br>";
-      echo "<br>";
-      break;
-    default:
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "Two names, one app. Which one is best, in your opinion? <br> <br>";
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "<br>";
-      echo "Which name works best for a quantitative app? <br>";
-      echo "(Click on your preferred choice) <br>";
-      echo "<br>";
-  }
-   $iter = $iter + 1;
+   $bgs = array("#FFFFFF", "#D9D9D9", "#A6A6A6");
+   $bg = rand(0,2);
+   $styles = array("white", "gray", "black");
+   echo "<link rel=\"stylesheet\" href=\".\\dhstyle_" . $styles[$bg] . ".css\">";
   ?>
-  <?php
-   $option1 = rand(0,10);
-   $option2 = rand(0,10);
-   while ($option2 == $option1) {
-      $option2 = rand(0,10);
-   }
-   echo "<a href = \".\\index.php?bg=$background&iter=$iter&opt=$option1\"> $options[$option1] <a> <br>\n";
-   echo "<a href = \".\\index.php?bg=$background&iter=$iter&opt=$option2\"> $options[$option2] <a> <br>\n";
-  ?>
- </center>
+ </head>
+
+ <body>
+
+  <h1>Name the app</h1>
+  <div class="grid-container">
+
+  <div class="grid-item-text">
+   You received this link because I want you to be part of a story. A story that needs a name. <br>
+   Picture in your mind an app that lets you measure what would otherwise be unmeasurable. <br>
+   An app that uses alternative methods to put a number in things like risk levels, future cashflows, or friends who will come to your party. <br>
+   Think of figure, values, quantities, sizes. Think of lateral thinking. <br>
+   You don't know exactly how it works, but can you picture it? <br>
+   Now give it a name. <br>
+   <br>
+   In the next page you will be presented with two names. Click on the one that best conveys the app you just imagined. Then you will be asked again, and again, and again, with different   options. <br>
+   <br>
+   Are you ready? <br>
+  </div>
+
+  <div class="grid-item-button">
+   <?php
+    echo "<a href = \".\\select.php?bg=$bg&iter=0&opt=0\">Continue</a>";
+   ?>
+  </div>
+
  </body>
+
 </html>
---
+++ select.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+
+ <head>
+  <?php
+   if (empty($_GET['bg'])){
+    $prevbg = 0;
+   } else {
+    $prevbg = $_GET['bg'];
+   }  
+   $iter = $_GET['iter'];
+   if (empty($_GET['iter'])){
+     $iter = 0;
+   } else {
+     $iter = $_GET['iter'];
+   }
+   if (empty($_GET['opt'])){
+    $opt = 0;
+   } else {
+    $opt = $_GET['opt'];
+   }
+   $bgs = array("#FFFFFF", "#D9D9D9", "#A6A6A6");
+   $bg = rand(0,2);
+   while (($bg == $prevbg)) {
+    $bg = rand(0,2);
+   }
+   $options = array("sharpseekentry","sharpentry","sizentry",
+                    "numeraria","numerana","numeranto",
+                    "numeriana","numerary","measurary",
+                    "numerama","oblyq");
+   $option1 = rand(0, 10);
+   $option2 = rand(0, 10);
+   while ($option2 == $option1){
+    $option2 = rand(0,10);
+   }
+   $progress = min(max($iter * 5, 0), 100);
+   $styles = array("white", "gray", "black");
+   echo "<link rel=\"stylesheet\" href=\".\\dhstyle_" . $styles[$bg] . ".css\">";
+  ?>
+ </head>
+
+ <body>
+
+  <h1>Name the app</h1>
+
+  <div class = "grid-container">
+  
+   <div class = "grid-item-text">
+    <?php
+     switch($iter) {
+      case 0:
+       echo "Between the two names suggested below, which one conveys best the app you just imagined? <br>";
+       echo "<br>\n";
+       break;
+      case 1:
+       echo $options[$opt] . " was a good choice. How about between these two? <br>";
+       echo "<br>\n";
+       break;
+      case 2:
+       echo "Thank you. This is really helpful. Keep doing this for as long as you find comfortable. <br>";
+       echo "<br>\n";
+       break;
+      case 20:
+       echo "I think I have enough answers from you. Thank you. But feel free to continue if you want. <br>";
+       echo "<br>\n";
+       break;
+      default:
+       echo "You are doing great. Keep going. <br>Which of these two names works best for a quantitative app, in your opinion? <br>";
+       echo "<br>\n";
+     }
+     $iter = $iter + 1;
+    ?>
+   </div>
+   <div class = "grid-item-button">
+    <?php
+     echo "<a href = \".\\select.php?bg=$bg&iter=$iter&opt=$option1\"> $options[$option1] </a> <br>\n";
+    ?>
+   </div>
+   <div class = "grid-item-button">
+    <?php
+     echo "<a href = \".\\select.php?bg=$bg&iter=$iter&opt=$option2\"> $options[$option2] </a> <br>\n";
+    ?>
+   </div>
+   <div class = "grid-item-text">
+    <?php
+     echo "Progres: " . ($iter-1) . "/20";
+    ?>
+    <div class="meter">
+     <?php
+      echo "<span style=\"width: " . $progress . "%\"></span>";
+     ?>
+    </div>
+   </div>
+  </div>
+ </body>
+<html>
